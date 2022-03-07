1e8f849482ef71d243958e39238e17650b675f03
frogindreams <frogharvard@gmail.com>
2022-03-07 12:59:35 +0700

    it's completely done

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,37 @@
 #include <string>
 
 using std::string;
+using std::cout;
+using std::cin;
 
 int main() {
     string sClear{ "String for clear function" };
 
     sClear.insert(0, 1, '>');
-    std::cout << sClear << '\n';
+    cout << sClear << '\n';
 
     sClear.erase(0, 1);
-    std::cout << sClear << '\n';
+    cout << sClear << '\n';
 
     sClear.clear();
-    std::cout << "is empty => " << sClear.empty() << '\n';
+    cout << "is empty => " << sClear.empty() << '\n';
+    cout << '\n';
+
+
+    string* arrayOfWords = new string[20];
+    char buf[10];
+
+    for (int iter = 0; iter < 20; ++iter) {
+        cout << iter << " => ";
+        cin.getline(buf, 10, '\n');
+        arrayOfWords[iter] = buf;
+    } cout << '\n';
+
+    for (int iter = 0; iter < 20; iter += 2) { 
+        if (arrayOfWords[iter] != "") {
+            cout << "<= " << arrayOfWords[iter] << '\n'; 
+        }
+    }
 
     return 0;
 }
